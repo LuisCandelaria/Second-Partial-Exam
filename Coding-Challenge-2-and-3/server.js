324479085fa813dc57e14d3fc40cc7cbaf60e8dd
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-05-08 08:16:31 -0500

    updating server.js

--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -5,9 +5,44 @@ const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
 
 const app = express();
+const {Sport} = require('./models/sport-model');
 
 
 /* Your code goes here */
+
+app.delete( '/sports/delete', jsonParser, (req, res) => {
+    console.log("Deleting a Sport.");
+    console.log(req.params);
+
+    let id = req.body.id;
+    let idP = req.params.id;
+
+    if( !id ) {
+        res.statusMessage = "The 'id' must be sent in the body.";
+        return res.status( 406 ).end();
+    }
+
+    /*if( id != idP ) {
+        res.statusMessage = "The 'id' in the body must be same as in the parameter.";
+        return res.status( 409 ).end();
+    }*/
+
+    Sport
+        .delete(id)
+            .then( result => {
+                if( !result ) {
+                    res.statusMessage = "The sport doesn't exist in the database.";
+                    return res.status( 404 ).end();
+                }
+                else {
+                    return res.status( 204 ).end();
+                }
+            })
+            .catch( err => {
+                res.statusMessage = "Something went wrong with the Database.";
+                return res.status( 500 ).end();
+            })
+});
 
 
 app.listen( PORT, () => {
