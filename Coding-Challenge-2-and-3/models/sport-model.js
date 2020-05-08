0e88a1a32b299267f244b541516258e90b654edc
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-05-08 08:15:55 -0500

    adding models

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -3,6 +3,43 @@ const uuid = require( 'uuid' );
 
 /* Your code goes here */
 
+const sportsSchema = mongoose.Schema({
+    id : {
+        type : String,
+        require : true,
+        unique : true
+    },
+    name : {
+        type : String,
+        require : true
+    },
+    num_players : {
+        type : Number,
+        require : true
+    }
+});
+
+const collection = mongoose.model( 'sports', sportsSchema);
+
+const Sport = {
+    delete : function (sportId) {
+        let filter = { id : sportId };
+        return collection
+            .deleteOne( filter )
+                .then( result => {
+                    if( result.n == 0) {
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                })
+                .catch( err => {
+                    return err;
+                })
+    }
+}
+
 module.exports = {
-    
+    Sport
 };
